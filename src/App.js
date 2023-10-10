826860704c668f1d4fb38ea30a10602e9718f6b3
Johan Blanco <johanbr2001@outlook.com>
2023-10-10 10:39:35 -0500

    pantalla 4 digitos

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Register from './components/Registrar/register'
 import Pastel from './components/pruebapaste/integracionpastel'
 import OlvidasteContraseña from './components/OlvidasteContraseña/OlvidasteContraseña'
 import HomePage from './components/HomePage/homepage';
-
+import CodOlviContra from './components/OlvidasteContraseña/CodOlviContra';
 const App = () => {
   return(
     <Router>
@@ -19,6 +19,7 @@ const App = () => {
     <Route path='/register/' element={<Register/>}/>
     <Route path='/pastel' element={<Pastel/>}/>
     <Route path='/olvidasteContraseña/' element={<OlvidasteContraseña/>}/>
+    <Route path='/CodOlviContra' element={<CodOlviContra/>}/>
     <Route path='/homepage/' element={<HomePage/>}/>
   </Routes>
   </Router>
