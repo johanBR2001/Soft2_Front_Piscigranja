dcf086e0b3ea20f857e6a520a39467eeb1a1b506
Dscarlett <danahscarlett@gmail.com>
2023-10-11 16:55:31 -0500

    UpdateDanah

--- src/components/EstadisticasGenerales/EstadGeneral.jsx
+++ src/components/EstadisticasGenerales/EstadGeneral.jsx
@@ -2,41 +2,40 @@ import React from 'react';
 import { ChartPolar } from './ChartPolar';
 import '../css/estiloChartPolar.css';
 import NavBar from '../HomePage/navbar.jsx';
-
+import LinesChart from './LinesChart';
 
 function EstadGeneral (){
     const containerStyle2 = {
         display: 'flex',
         alignItems: 'center', // Alinea verticalmente al centro
-        padding: '250px', // Espaciado alrededor del contenido
+        padding: '50px', // Espaciado alrededor del contenido
+        // justifyContent: 'space-between',
     };
     const titleStyle = {
         paddingLeft: '10px',
         paddingRight: '100px',
     };
     return(
-        <div >
+       
         <div className="contenedor">
             <NavBar />
             <div className="label"style={{...containerStyle2,titleStyle}}>
                 <div>
                     <h1 >Estadisticas Generales </h1>
-                <div >
                     <h1 id='fuente'>Estado de las especies </h1>
                 </div>
-                </div>
-                <div id='graphPolar' style={{maxWidth: '300px'}}>
+                <div  style={{maxWidth: '300px'}}>
                     <ChartPolar />
                 </div>
                 {/* <div class="box">
                     <div class="rectangle" />
                 </div> */}
             </div>
-
-        
+            <div  style={containerStyle2}>
+                <LinesChart/>
+            </div>
         </div>
-        
-    </div> 
+
     );
 }
 
---
+++ src/components/EstadisticasGenerales/LinesChart.js
@@ -0,0 +1,63 @@
+import { Line } from 'react-chartjs-2';
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler,
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler
+);
+
+var beneficios = [0, 56, 20, 36, 80, 40, 30, -20, 25, 30, 12, 60];
+var meses = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+
+var midata = {
+    labels: meses,
+    datasets: [ // Cada una de las líneas del gráfico
+        {
+            label: 'Semana',
+            data: beneficios,
+            tension: 0.5,
+            fill : true,
+            borderColor: 'rgba(55, 155, 177, 0.53)',
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            pointRadius: 5,
+            pointBorderColor: 'rgba(122, 204, 222, 0.53)',
+            pointBackgroundColor: 'rgba(55, 155, 177, 0.53)',
+        },
+        {
+            label: 'Otra línea',
+            data: [20, 25, 60, 65, 45, 10, 0, 25, 35, 7, 20, 25]
+        },
+    ],
+};
+
+var misoptions = {
+    scales : {
+        y : {
+            min : 0
+        },
+        x: {
+            ticks: { color: 'rgba(55, 155, 177, 0.53)'}
+        }
+    }
+};
+
+export default function LinesChart() {
+    return <Line data={midata} options={misoptions}/>
+}
