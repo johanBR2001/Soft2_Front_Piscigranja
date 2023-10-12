3820040c6530d291a1f2dffde89624c82e5304ea
johanBR2001 <johanbr2001@outlook.com>
2023-10-11 19:51:35 -0500

    concatenación de numeros para el olvido de contraseña

--- src/components/OlvidasteContraseña/CodOlviContra.jsx
+++ src/components/OlvidasteContraseña/CodOlviContra.jsx
@@ -1,6 +1,22 @@
 import '../css/estiloCodOlviContra.css'
-
+import React, { useState } from 'react';
 function CodOlviContra() {
+
+    const [num1, setnum1] = useState('');
+    const [num2,setnum2] = useState('');
+    const [num3,setnum3] = useState('');
+    const [num4,setnum4]= useState('');
+    const handleButtonClick = () => {
+        console.log('numero1', num1);
+        console.log('numero2', num2);
+        console.log('numero3', num3);
+        console.log('numero4', num4);
+        const resultado=num1+num2+num3+num4;
+        console.log('la concatenación es: ',resultado)
+        alert(resultado)
+
+
+      };
     return <div>
 
         <link rel="stylesheet" href="index.css" />
@@ -17,22 +33,31 @@ function CodOlviContra() {
                                 <div>Ingresa la clave enviada a tu correo</div>
                                 <div className='cubitos'>
                                     <div>
-                                        <input type="text" class="form-control" />
+                                        <input type="text" class="form-control"
+                                        value={num1}
+                                        onChange={(e) => setnum1(e.target.value)} />
                                     </div>
                                     <div>
-                                        <input type="text" class="form-control" />
+                                        <input type="text" class="form-control" 
+                                        value={num2}
+                                        onChange={(e) => setnum2(e.target.value)}/>
                                     </div>
                                     <div>
-                                        <input type="text" class="form-control" />
+                                        <input type="text" class="form-control" 
+                                        value={num3}
+                                        onChange={(e) => setnum3(e.target.value)}/>
                                     </div>
                                     <div>
-                                        <input type="text" class="form-control" />
+                                        <input type="text" class="form-control" 
+                                        value={num4}
+                                        onChange={(e) => setnum4(e.target.value)}/>
                                     </div>
                                 </div>
                                 <div>
-                                    <button className='btn_CodOlviContra'>Siguiente</button>
+                                    <button className='btn_CodOlviContra' onClick={handleButtonClick}>Siguiente</button>
                                 </div>
                             </form>
+                            
                         </div>
                     </div>
                 </div>
