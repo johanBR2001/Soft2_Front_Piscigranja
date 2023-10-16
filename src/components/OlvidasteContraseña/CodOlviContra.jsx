b8e06757bae734cc0884728f49afdc5c13651135
Renzuki123 <renzosaucedo@hotmail.com>
2023-10-16 13:45:15 -0500

    pusheando

--- src/components/OlvidasteContraseña/CodOlviContra.jsx
+++ src/components/OlvidasteContraseña/CodOlviContra.jsx
@@ -1,22 +1,54 @@
-import '../css/estiloCodOlviContra.css'
+import '../css/estiloCodOlviContra.css';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 function CodOlviContra() {
 
     const [num1, setnum1] = useState('');
     const [num2,setnum2] = useState('');
     const [num3,setnum3] = useState('');
     const [num4,setnum4]= useState('');
-    const handleButtonClick = () => {
-        console.log('numero1', num1);
-        console.log('numero2', num2);
-        console.log('numero3', num3);
-        console.log('numero4', num4);
-        const resultado=num1+num2+num3+num4;
-        console.log('la concatenación es: ',resultado)
-        alert(resultado)
+    let resultado = ''
 
+      const navigate = useNavigate()
+      const [error, setError] = useState(null);
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          console.log('numero1', num1);
+          console.log('numero2', num2);
+          console.log('numero3', num3);
+          console.log('numero4', num4);
+          resultado=num1+num2+num3+num4;
+          console.log('la concatenación es: ',resultado)
+          alert(resultado)
 
-      };
+          const response = await fetch("http://127.0.0.1:8000/backend/verificar_codigo/", {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              resultado: resultado
+            })
+          });
+          const data = await response.json();
+          console.log(data)
+          if(data.error===""){
+            const jsonData = JSON.stringify(data.restaurante);
+            sessionStorage.setItem('data', jsonData);
+            navigate('/2023-0-pw-entregable-2/bienvenida');
+          }
+          else{
+            setError("Sus credednciales son incorrectas")
+          }
+         
+          // redirigir al usuario a la página principal
+        } catch (err) {
+          console.log("no ingreso")
+          setError(err.message);
+        }
+      }
+
     return <div>
 
         <link rel="stylesheet" href="index.css" />
@@ -54,7 +86,7 @@ function CodOlviContra() {
                                     </div>
                                 </div>
                                 <div>
-                                    <button className='btn_CodOlviContra' onClick={handleButtonClick}>Siguiente</button>
+                                    <button className='btn_CodOlviContra' onClick={handleSubmit}>Siguiente</button>
                                 </div>
                             </form>
                             
