ce67e464cb195433b420007b67912885c0197096
Renzuki123 <renzosaucedo@hotmail.com>
2023-10-16 20:49:26 -0500

    ya se muestra las estadísticas

--- src/components/Charts/ChartPolar.js
+++ src/components/Charts/ChartPolar.js
@@ -1,47 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { PolarArea } from 'react-chartjs-2';
+import Chart from 'chart.js/auto'; // Importa Chart desde 'chart.js/auto'
 
 export function ChartPolar() {
-  const [chartData, setChartData] = useState({
-    labels: [],
-    datasets: [
-      {
-        label: '# elementos',
-        data: [],
-        backgroundColor: [],
-        borderWidth: 1,
-      },
-    ],
-  });
+  const [chartData, setChartData] = useState({ labels: [], datasets: [] });
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch("http://127.0.0.1:8000/backend/obtener_conteo_materiales/");
-        if (response.ok) {
-          const data = await response.json();
-          const labels = data.map((item) => item.nombre);
-          const datasetData = data.map((item) => item.cantidad);
-          const backgroundColors = data.map((item, index) => `rgba(${index * 20}, 99, 132, 0.5)`);
-          
-          setChartData({
-            labels,
-            datasets: [
-              {
-                label: '# elementos',
-                data: datasetData,
-                backgroundColor: backgroundColors,
-                borderWidth: 1,
-              },
-            ],
-          });
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
+    // Aquí hacemos una solicitud al endpoint de Django para obtener los datos
+    fetch("http://localhost:8000/backend/obtener_conteo_materiales/") // Asegúrate de usar la URL correcta
+      .then((response) => response.json())
+      .then((data) => {
+        // Procesamos los datos recibidos para el gráfico
+        const labels = data.map((item) => item.nombre);
+        const values = data.map((item) => item.cantidad);
 
-    fetchData();
+        const newChartData = {
+          labels: labels,
+          datasets: [
+            {
+              label: '# elementos',
+              data: values,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        };
+
+        setChartData(newChartData);
+      });
   }, []);
 
   return <PolarArea data={chartData} />;
