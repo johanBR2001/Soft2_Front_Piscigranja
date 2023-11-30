ef0019c2bd85b69bf1ecd5c3782e287bff5d76ea
Renzuki123 <renzosaucedo@hotmail.com>
2023-11-29 20:04:57 -0500

    nose

--- src/components/Soporte/card.jsx
+++ src/components/Soporte/card.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
 
 function Card({titulo, imagenSrc, peces,capacidad,salud}) {
+    const handleSolicitarLimpieza = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:8000/backend/solicitar_limpieza/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({}),
+            });
+
+            if (response.ok) {
+                // Manejar la respuesta exitosa
+                console.log('Correo enviado con éxito');
+            } else {
+                // Manejar la respuesta de error
+                console.error('Error al enviar el correo');
+            }
+        } catch (error) {
+            console.error('Error de red:', error);
+        }
+    };
+
+
     return (
         <div class="card">
             <div class="row" id="titulo">
@@ -17,7 +40,7 @@ function Card({titulo, imagenSrc, peces,capacidad,salud}) {
                         <p class="card-text">{salud}</p>
                         <div className='d-flex justify-content-center'>
                         {salud=="Salud del estanque Malo"? (
-      <a href="#" className="btn btn-primary">Solicitar limpieza</a>
+      <a href="#" className="btn btn-primary" onClick={handleSolicitarLimpieza}>Solicitar limpieza</a>
     ) : (
         <button href="#" className="btn btn-primary" disabled>Solicitar limpieza</button>
     )}
